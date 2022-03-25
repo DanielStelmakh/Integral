b9c5aa52eaf10c76e2ef7a4799d4addb840dff18
HP <daniel,teshkin@mail.ru>
2022-03-25 23:28:03 +0300

    Load

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <valarray>
 
 using namespace std;
+float step(double a,double b,double n);
 float sq(float x);
-float integrals(float x,float h,float n);
+float integrals(float x,float h,double n);
 float Function(float x);
-float trap(float x,float h,float n);
-float simpson(float x,float h,float n);
+float trap(float x,float h,double n);
+float simpson(float x,float h,double n);
 int main() {
-    float n;
+    double n,a,b;
     cin >> n;
-    float h = 2.0 / n;
-    cout << h << endl;
-    float i=-1+h/2;
-    float I = integrals(i,h,n);
-    float U=trap(i,h,n);
-   float z= simpson(i,h,n);
+    cin>>a;
+    cin>>b;
+    float h = step(a,b,n);
+    float f=-1+h/2;
+    float I = integrals(f,h,n);
+    float U=trap(f,h,n);
+   float z= simpson(f,h,n);
     cout<<"Метод прямоугольников"<<" "<<I<<endl;
     cout<<"Метод трапеций"<<" "<<U<<endl;
     cout<<z<<"Симпсон"<<" "<<endl;
@@ -30,7 +32,7 @@ float Function(float x){
 float sq(float x){
     return x*x;
 }
-float integrals(float x,float h,float n) {
+float integrals(float x,float h,double n) {
     float S = 0.0;
     for (int i = 0; i < n; i++) {
             if(x<1.0){
@@ -40,7 +42,7 @@ float integrals(float x,float h,float n) {
     for (int i=2;i<10;i+=12)S+=Function(1-h/i)* h;
     return S;
 }
-float trap(float x,float h,float n){
+float trap(float x,float h,double n){
 
     float S = 0.0;
     for (int i = 0; i < n; i++) {
@@ -50,7 +52,7 @@ float trap(float x,float h,float n){
     } for (int i=2;i<10;i+=8)S+=Function(1-h/i)* h;
     return S;
 }
-float simpson(float x,float h,float n){
+float simpson(float x,float h,double n){
     float S = 0.0;
 
     for (int i = 0; i < n; i+=2) {
@@ -62,3 +64,7 @@ float simpson(float x,float h,float n){
     for (int i=2;i<10;i+=4)S+=Function(1-h/i)* h;
     return S;
 }
+float step(double a,double b,double n){
+    return  (b-a)/n;
+}
+
