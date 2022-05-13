a9dc0567d12cf2ed2fc61a55c129af7c39f665e3
HP <daniel,teshkin@mail.ru>
2022-05-13 21:45:47 +0300

    Load

--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 #include <valarray>
 
 using namespace std;
-float step(double a,double b,double n);
 float sq(float x);
-float integrals(float x,float h,double n);
+float integrals(float x,float h,float n);
 float Function(float x);
-float trap(float x,float h,double n);
-float simpson(float x,float h,double n);
+float trap(float x,float h,float n);
+float simpson(float x,float h,float n);
 int main() {
     double n,a,b;
     cin >> n;
     cin>>a;
     cin>>b;
-    float h = step(a,b,n);
+    float h = (b-a)/ n;
+    cout << h << endl;
     float f=-1+h/2;
     float I = integrals(f,h,n);
     float U=trap(f,h,n);
@@ -32,39 +32,35 @@ float Function(float x){
 float sq(float x){
     return x*x;
 }
-float integrals(float x,float h,double n) {
+float integrals(float x,float h,float n) {
     float S = 0.0;
     for (int i = 0; i < n; i++) {
-            if(x<1.0){
-            S += (Function(x) * h);
-            x += h;}
+
+            S += Function(x) ;
+            x += h;
             }
-    for (int i=2;i<10;i+=12)S+=Function(1-h/i)* h;
+    for (int i=2;i<10;i+=12)S+=Function(1-h/i);
+    S*=h;
     return S;
 }
-float trap(float x,float h,double n){
+float trap(float x,float h,float n){
 
     float S = 0.0;
     for (int i = 0; i < n; i++) {
-            if(x<1.0&&x+h<1.0){
-            S += (Function(x)+Function(x+h))/2 *h;
-            x += h;}
-    } for (int i=2;i<10;i+=8)S+=Function(1-h/i)* h;
+        S += (Function(x)+Function(x+h))/2 ;
+            x += h;
+    } for (int i=2;i<10;i+=8)S+=Function(1-h/i);
+    S*=h;
     return S;
 }
-float simpson(float x,float h,double n){
+float simpson(float x,float h,float n){
     float S = 0.0;
 
     for (int i = 0; i < n; i+=2) {
-        if(x<1.0 && x+h<1.0&& x+2*h<1.0){}
-            S += (Function(x)+4*Function(x+h)+ Function(x+2*h)) *h/3;
-            x += h;
-        }
 
-    for (int i=2;i<10;i+=4)S+=Function(1-h/i)* h;
+            S += (Function(x)+4*Function(x+h)+ Function(x+2*h));
+            x += h;}
+    for (int i=2;i<10;i+=4)S+=Function(1-h/i);
+    S*=h/3;
     return S;
 }
-float step(double a,double b,double n){
-    return  (b-a)/n;
-}
-
